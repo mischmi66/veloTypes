2a056f16759c39e3bba6fa4da0ed0032be3d204c
mischmi66 <mischmi1966@gmail.com>
2021-12-02 17:20:27 +0100

    add bootstrap menu

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import logo from './logo.gif';
 import './App.css';
 import { client } from './client';
 import Posts from './components/Posts';
+import 'bootstrap/dist/css/bootstrap.css'
+import { Nav, Navbar, NavDropdown } from 'react-bootstrap'
+import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse';
+import NavbarToggle from 'react-bootstrap/esm/NavbarToggle';
 
 class App extends React.Component {
   state = {
@@ -27,7 +32,28 @@ componentDidMount(){
         <div className='container'>
           <header>
             <div className='wrapper'>
-              <span>Velos from Contentful</span>
+              <Navbar bg="mygrey" variant="light" sticky="top" expand="md" >
+              
+              <Navbar.Brand>
+            <img src={logo} alt="Logo Velo" />
+                Velos from Contentful
+              </Navbar.Brand>
+              <NavbarToggle />
+              <NavbarCollapse>
+              <Nav>
+              
+              <NavDropdown title="Menu">
+              <NavDropdown.Item href='Velos/Klapprad'>Start</NavDropdown.Item>
+              <NavDropdown.Item href='Velos/Klapprad'>Über Uns</NavDropdown.Item>
+                <NavDropdown.Item href='Velos/Klapprad'>Klapprad</NavDropdown.Item>
+                <NavDropdown.Item href='Velos/Mountain'>Mountainbike</NavDropdown.Item>
+                <NavDropdown.Item href='Velos/Klapprad'>Rennrad</NavDropdown.Item>
+              </NavDropdown>
+              
+                
+              </Nav>
+              </NavbarCollapse>
+              </Navbar>
             </div>
           </header>
           <main><div className='wrapper'>
